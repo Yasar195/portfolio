e1cb7577aeaf65218ae23ef4eac32ff3ba8bd763
Yasar195 <imyasar07@gmail.com>
2022-07-24 09:34:43 +0530

    fixed copy

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,9 +1,19 @@
 import { CgMail } from 'react-icons/cg';
 import { motion } from 'framer-motion';
 import '../styles/Email.css';
-
+import { useState } from 'react';
 
 const Email = () => {
+
+
+    const [text, setText] = useState('yasar07@gmail.com')
+
+    const copy = () => {
+        navigator.clipboard.writeText("imyasar07@gmail.com");
+        setText("Mail id copied")
+        setTimeout(()=> setText("yasar07@gmail.com"), 2000);
+    }
+
     return( 
         <motion.div
             whileHover={{
@@ -11,14 +21,14 @@ const Email = () => {
                 cursor: 'pointer',
                 backgroundColor: 'rgba(237, 214, 0, 0.5)'
             }}
-        
+            onClick={copy}
         >
             <div className='email'>
                 <div className="comp glogo">
                     <CgMail size={"30px"}/>
                 </div>
                 <div className="comp id">
-                    <p>imyasar07@gmail.com</p>
+                    <p>{text}</p>
                 </div>
             </div>
         </motion.div>

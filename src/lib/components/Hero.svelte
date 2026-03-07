a11ddf5621c261cd45f80b3b0fc68ad5055e8668
Yasar195 <imyasar07@gmail.com>
2026-03-07 20:40:03 +0530

    update

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -41,7 +41,7 @@
       </p>
       
       <div class="cta-buttons">
-        <a href="#projects" class="btn btn-primary">
+        <a href="https://projects.yasararafath.in" class="btn btn-primary">
           View My Work
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <line x1="5" y1="12" x2="19" y2="12"></line>
